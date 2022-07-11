83974762e33c1ed596af6aaf3d11a929ca3872b6
ItamarSahar <91605437+ItamarSahar@users.noreply.github.com>
2022-07-11 14:23:57 +0300

    Add files via upload

--- src/cmps/LoginCmp/LoginCmp.test.js
+++ src/cmps/LoginCmp/LoginCmp.test.js
@@ -1,89 +1,93 @@
 // https://enzymejs.github.io/enzyme/
-
-import {shallow, mount} from 'enzyme';
+//*Testing For Components !
+//! allow us to have a virtual DOM and virtual CMP- NOT renderng child components !
+//! ONLY in REACT 17 !
+import { shallow, mount } from 'enzyme'
 
 import LoginCmp from './LoginCmp'
 
 describe('LoginCmp', () => {
-  const mockUser = {
-    username: 'puki',
-    password: '123456'
-  }
+	const mockUser = {
+		username: 'puki',
+		password: '123456',
+	}
 
-  describe('submit btn', () => {
-    it('should have disabled attribute when inputs are empty', () => {
-      expect.assertions(2);
-      const wrapper = shallow(<LoginCmp />)
-  
-      const btnWrapper = wrapper.find('.btn-submit')
-      expect(btnWrapper.hasClass('is-block')).toBe(true)
-      expect(btnWrapper.props()["disabled"]).toBe(true)
-    })
+	describe('submit btn', () => {
+		it('should have disabled attribute when inputs are empty', () => {
+			//?it and test are the same !!!
+			expect.assertions(2)
+			const wrapper = shallow(<LoginCmp />)
 
-    it('should have disabled attribute when password is empty', async () => {
-      expect.assertions(2);
-      const wrapper = shallow(<LoginCmp />) // shallow - no need to mount the children cmps
-      const user = {
-        username: 'puki',
-        password: ''
-      }
+			const btnWrapper = wrapper.find('.btn-submit')
+			expect(btnWrapper.hasClass('is-block')).toBe(true)
+			expect(btnWrapper.props()['disabled']).toBe(true)
+		})
 
-      wrapper.setState(user)
+		it('should have disabled attribute when password is empty', async () => {
+			expect.assertions(2)
+			const wrapper = shallow(<LoginCmp />) // shallow - no need to mount the children cmps
+			const user = {
+				username: 'puki',
+				password: '',
+			}
 
-      const btnWrapper = wrapper.find('.btn-submit')
-      expect(btnWrapper.hasClass('is-block')).toBe(true)
-      expect(btnWrapper.props()["disabled"]).toBe(true)
-    })
+			wrapper.setState(user)
 
-    it('should have disabled attribute when username is empty', async () => {
-      expect.assertions(2);
-      const wrapper = shallow(<LoginCmp />) // shallow - no need to mount the children cmps
-      const user = {
-        username: '',
-        password: '123456'
-      }
+			const btnWrapper = wrapper.find('.btn-submit')
+			expect(btnWrapper.hasClass('is-block')).toBe(true)
+			expect(btnWrapper.props()['disabled']).toBe(true)
+		})
 
-      wrapper.setState(user)
+		it('should have disabled attribute when username is empty', async () => {
+			expect.assertions(2)
+			const wrapper = shallow(<LoginCmp />) // shallow - no need to mount the children cmps
+			const user = {
+				username: '',
+				password: '123456',
+			}
 
-      const btnWrapper = wrapper.find('.btn-submit')
-      expect(btnWrapper.hasClass('is-block')).toBe(true)
-      expect(btnWrapper.props()["disabled"]).toBe(true)
-    })
+			wrapper.setState(user)
 
-    it('should not have disable attribute submit btn', () => {
-      expect.assertions(2);
+			const btnWrapper = wrapper.find('.btn-submit')
+			expect(btnWrapper.hasClass('is-block')).toBe(true)
+			expect(btnWrapper.props()['disabled']).toBe(true)
+		})
 
-      const wrapper = shallow(<LoginCmp />) // shallow - no need to mount the children cmps
-      const user = {
-        username: 'puki',
-        password: '123456'
-      }
+		it('should not have disable attribute submit btn', () => {
+			expect.assertions(2)
 
-      wrapper.setState(user)
+			const wrapper = shallow(<LoginCmp />) // shallow - no need to mount the children cmps
+			const user = {
+				username: 'puki',
+				password: '123456',
+			}
 
-      const btnWrapper = wrapper.find('.btn-submit')
-      expect(btnWrapper.hasClass('is-block')).toBe(false)
-      expect(btnWrapper.props()["disabled"]).toBe(false)
-    })
-  })
+			wrapper.setState(user)
 
-  it('should trigger submit event with the user details', async () => {
-    expect.assertions(3);
-    const onSubmitMock = jest.fn(val => val) // get a mock function
-    const wrapper = mount(<LoginCmp onSubmit={onSubmitMock} />) // mount - we need to render the nested 'input-cmp' cmps
+			const btnWrapper = wrapper.find('.btn-submit')
+			expect(btnWrapper.hasClass('is-block')).toBe(false)
+			expect(btnWrapper.props()['disabled']).toBe(false)
+		})
+	})
 
-    const textInput = wrapper.find('input[name="username"]')
-    textInput.simulate('change', { target: { value: mockUser.username } });
+	it('should trigger submit event with the user details', async () => {
+		expect.assertions(3)
+		const onSubmitMock = jest.fn((val) => val) // get a mock function
+		const wrapper = mount(<LoginCmp onSubmit={onSubmitMock} />) // mount - we need to render the nested 'input-cmp' cmps
 
-    const passwordInput = wrapper.find('input[name="password"]')
-    passwordInput.simulate('change', { target: { value: mockUser.password } });
+		const textInput = wrapper.find('input[name="username"]') //? Select the elemnt in the mock component
+		textInput.simulate('change', { target: { value: mockUser.username } }) //? The event odj
 
-    wrapper.find("form").simulate('submit');
+		const passwordInput = wrapper.find('input[name="password"]')
+		passwordInput.simulate('change', { target: { value: mockUser.password } })
 
-    expect(onSubmitMock).toHaveBeenCalled()
+		wrapper.find('form').simulate('submit')
 
-    const resVal = onSubmitMock.mock.results[0].value
-    expect(resVal.username).toBe(mockUser.username)
-    expect(resVal.password).toBe(mockUser.password)
-  })
+		expect(onSubmitMock).toHaveBeenCalled() //? To know the submit function has been called !
+
+		//! Check that the Submit function got the currect Value !
+		const resVal = onSubmitMock.mock.results[0].value
+		expect(resVal.username).toBe(mockUser.username)
+		expect(resVal.password).toBe(mockUser.password)
+	})
 })
